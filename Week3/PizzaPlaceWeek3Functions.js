308db24ac4bf5ff74af48be8d270b632ffa23d9f
Doug Brockel <workmail.jsph@gmail.com>
2023-01-26 19:16:18 -0600

    Cleaned JS

--- Week3/PizzaPlaceWeek3Functions.js
+++ Week3/PizzaPlaceWeek3Functions.js
@@ -1,14 +1,15 @@
-let pizzaToppings = ["Pepperoni","Red-Peppers","Sausage","Canadian-Bacon","Cheese"]; //Kinda had to add cheese as a topping so it wouldn't flag on my listToppings function
+let pizzaToppings = ["Pepperoni", "Red-Peppers", "Sausage", "Canadian-Bacon", "Cheese"]; //Kinda had to add cheese as a topping so it wouldn't flag on my listToppings function
 
-function listToppings(Toppings){
+
+function listToppings(Toppings) {
     let txt = '';
-    for (let x in Toppings){
+    for (let x in Toppings) {
         let Test = (V) => {return V == Toppings[x]}; //declare function for our .find
         let checkit = pizzaToppings.find(Test); //.find on pizzatoppings method runs and jumps to Test
-        if (checkit == undefined){ //If one of the items in our 'test' on checkit comes back as undefined, print it doesn't exist.
-          return  false;
+        if (checkit == undefined) { //If one of the items in our 'test' on checkit comes back as undefined, print it doesn't exist.
+            return false;
         }
-        if (x == Toppings.length-1){
+        if (x == Toppings.length - 1) {
             txt += (" And... ")
         }
         txt += (Toppings[x]);
@@ -18,59 +19,67 @@ function listToppings(Toppings){
 }
 
 
-function greetCustomer(){
+function greetCustomer() {
     console.log("Welcome to Johnny's Pizza Palace! Our toppings are; ")
-    for (let i of pizzaToppings){
+    for (let i of pizzaToppings) {
         console.log(i);
     }
 };
 
-function getPizzaOrder(size,crust, ...toppings){
-    if (toppings.length === 0 ){
+
+function getPizzaOrder(size, crust, ...toppings) {
+    if (toppings.length === 0) {
         console.log(`One ${size} ${crust} crust cheese pizza.`);
         toppings.push("Cheese");
-    }else{
+    } else {
         console.log(`One ${size} ${crust} crust Pizza coming up with`);
     }
     let tst = listToppings(toppings);
-    if (tst === false){
-         console.log("PLEASE REORDER, that is not a topping.")
+    if (tst === false) {
+        console.log("PLEASE REORDER, that is not a topping.")
         return null;
     }
-    let pizza = [size,crust,toppings];
+    let pizza = [size, crust, toppings];
     return pizza
 };
 
-function preparePizza(Arr){
+
+function preparePizza(Arr) {
     class PALACEPIZZA {
-        constructor(size,crust,toppings,Pizza){
+        constructor(size, crust, toppings, Pizza) {
             Pizza = {};
             Pizza.size = size;
             Pizza.crust = crust;
             Pizza.toppings = toppings;
             this.Pizza = Pizza;
         }
-    add(Val,Name){
-        this.Pizza[Val] = Name;
+        add(Val, Name) {
+            this.Pizza[Val] = Name;
         }
-    list(){
-        console.log(this.Pizza);
+        list() {
+            console.log(this.Pizza);
+        }
     }
-    }
-    let FirstPizza = new PALACEPIZZA(Arr[0],Arr[1],Arr[2]); // use class to construct a new pizza
+    let FirstPizza = new PALACEPIZZA(Arr[0], Arr[1], Arr[2]); // use class to construct a new pizza
     console.log("Pizza is Cookin'...");
     FirstPizza.list();
     return FirstPizza.Pizza;
 };
 
-function servePizza(PizzaOrder){
+
+function servePizza(PizzaOrder) {
     let Size = PizzaOrder.size;
     let Crust = PizzaOrder.crust;
     let toppings = PizzaOrder.toppings;
     console.log(`Here's your ${Size} ${Crust} crust Pizza with`);
     listToppings(toppings);
 }
+
+
 greetCustomer();
-let Order = getPizzaOrder("Large","Thick");
+
+let Order = getPizzaOrder("Large", "Thick");
+
 let Making = preparePizza(Order);
+
 let final = servePizza(Making);
